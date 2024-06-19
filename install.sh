cc7106ca181545199dc8b42707a4e6a828b9b8db
Daniel Conrad <3791768+danielmconrad@users.noreply.github.com>
2024-06-19 16:38:51 -0500

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+if [ uname -s = "Darwin ]; then
+  if [ ! -e $(xcode-select --print-path &> /dev/null) ]; then xcode-select --install &> /dev/null; fi
+  until [ -e $(xcode-select --print-path &> /dev/null) ]; do sleep 5; done
+fi
+
+if command -v wget &> /dev/null; then
+  sh -c "$(wget -qO- get.chezmoi.io)" -- init --apply danielmconrad
+else
+  sh -c "$(curl -fsLS get.chezmoi.io)" -- init --apply danielmconrad
+fi
