3bd428ba893a99b8fb6d6970fdfdc6804974af0d
Daniel Conrad <3791768+danielmconrad@users.noreply.github.com>
2024-06-19 14:55:06 -0500

    Update utilities.sh

--- scripts/utilities.sh
+++ scripts/utilities.sh
@@ -19,28 +19,28 @@ log() {
 
 install_brew() {
   log "Installing $@ via brew"
-  brew uninstall $1 2> /dev/null
+  brew uninstall $1 &>/dev/null
   echo "$1 uninstalled"
   brew install -f $1
 }
 
 install_brew_cask() {
   log "Installing $1 via brew cask"
-  brew uninstall --cask 2> /dev/null
+  brew uninstall --cask &>/dev/null
   echo "$1 uninstalled"
   brew install --cask -f $@
 }
 
 install_apt() {
   log "Installing $1 via apt"
-  sudo apt-get uninstall $1 2> /dev/null
+  sudo apt-get uninstall $1 &>/dev/null
   echo "$1 uninstalled"
   sudo apt-get install -y $@
 }
 
 install_snap() {
   log "Installing $1 via snap"
-  sudo snap remove $1 2> /dev/null
+  sudo snap remove $1 &>/dev/null
   echo "$1 uninstalled"
   sudo snap install $@
 }
